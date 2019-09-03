b783008c8f3af057b451589d821af69f25610d06
lijiacheng14 <lijiacheng14@countrygarden.com.cn>
2019-09-03 18:50:48 +0800

    update 增加loading按钮组件注释说明

--- src/components/Botton/LoadingBtn.vue
+++ src/components/Botton/LoadingBtn.vue
@@ -1,29 +1,28 @@
 <!-- 加载中的按钮 用于提交，登陆等 -->
 <template>
-<div>
-    <a-button
-    :loading="loading"
-    v-bind="$attrs"
-    v-on="$listeners"
-    :disabled="loading"
-    >登录</a-button>
-</div>
+    <div>
+        <a-button
+            :loading="loading"
+            v-bind="$attrs"
+            v-on="$listeners"
+            :disabled="loading"
+        >登录</a-button>
+    </div>
 </template>
 
 <script>
 export default {
     //可以传入 ant-vue  a-button的配置项 (v-bind="$attrs" v-on="$listeners" 可进行接收属性和事件)
     //另外可再传入loading 用于显示加载动画和禁用按钮
-    //@example  <a-button :loading="true" type="danger"></a-button>
-    props:{
-        loading:{
+    //@example  <a-button :loading="true" type="danger" @click="event"></a-button>
+    props: {
+        loading: {
             type: Boolean,
             value: false
         }
     }
-}
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
