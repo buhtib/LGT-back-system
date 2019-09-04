14838964d3d6ef0e80cbc015fbc3f08ea1c69530
lijiacheng14 <lijiacheng14@countrygarden.com.cn>
2019-09-04 14:36:09 +0800

    modify 替换 阿里图标js地址路径

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -9,4 +9,4 @@ export function timeFix () {
 }
 
 //阿里图标管理 js路径
-export const scriptUrl =  '//at.alicdn.com/t/font_1319336_m1dv3f6q18f.js';
+export const scriptUrl =  '//at.alicdn.com/t/font_1360177_e26ksaw7cvk.js';
